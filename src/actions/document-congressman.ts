49a6ff4a90c88fb4d14813a33ab3f5ac654746ba
Einar Valur Aðalsteinsson <fizk78@gmail.com>
2019-03-28 15:14:54 +1100

    Adds all proponents to issue (#3)
    * Adds all proponents to issue
    
    * Fixes tests
    
    * Sorts documents

--- src/actions/document-congressman.ts
+++ src/actions/document-congressman.ts
@@ -45,77 +45,46 @@ export const addProponentDocument = async (message: Message<CongressmanDocument>
 };
 
 /**
- * If document is the initial document and the congressman is the first proponent, then add him/her to the Issue
+ * If document is the initial document, add the proponents to the Issue
  *
  * @param message
  * @param mongo
  * @param client
  */
 export const addProponentIssue = async (message: Message<CongressmanDocument>, mongo: Db, client: HttpQuery): Promise<any> => {
-    const documents: {current: Document, all: Document[]} = await Promise.all([
+    const documents: {current: Document, all: Document[], congressmen: CongressmanDocument[]} = await Promise.all([
         client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol/${message.body.document_id}`),
         client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol`),
-    ]).then(([current, all]) => ({current, all}));
+        client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol/${message.body.document_id}/thingmenn`),
+    ]).then(([current, all, congressmen]) => ({current, all, congressmen}));
 
-    const congressman: {congressman: Congressman, constituency: Constituency, party: Party} = await Promise.all([
-        client(`/samantekt/thingmenn/${message.body.congressman_id}`),
-        client(`/samantekt/thingmenn/${message.body.congressman_id}/kjordaemi`, {dags: documents.current.date}),
-        client(`/samantekt/thingmenn/${message.body.congressman_id}/thingflokkar`, {dags: documents.current.date}),
-    ]).then(([congressman, constituency, party]) => ({congressman, constituency, party}));
-
-    if (documents.all && documents.all[0] && documents.all[0].document_id === message.body.document_id && message.body.order === 1) {
-        return mongo.collection('issue')
-            .updateOne({
-                'issue.assembly_id': message.body.assembly_id,
-                'issue.issue_id': message.body.issue_id,
-                'issue.category': message.body.category,
-            }, {
-                $set: {proponent: congressman}
-            }, {
-                upsert: true
-            }).then(result => {
-                if (!result.result.ok) {
-                    throw new Error(`ERROR: DocumentCongressman.addProponentIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`);
-                }
-                return `DocumentCongressman.addProponentIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`;
+    if (documents.all && documents.all[0] && documents.all[0].document_id === message.body.document_id) {
+        return await Promise.all(documents.congressmen.map(congressmanDocument => {
+            return Promise.all([
+                client(`/samantekt/thingmenn/${congressmanDocument.congressman_id}`),
+                client(`/samantekt/thingmenn/${congressmanDocument.congressman_id}/kjordaemi`, {dags: documents.current.date}),
+                client(`/samantekt/thingmenn/${congressmanDocument.congressman_id}/thingflokkar`, {dags: documents.current.date}),
+                congressmanDocument,
+            ])
+        })).then(all => all.map(([congressman, constituency, party, document]) => ({congressman, constituency, party, document})))
+            .then(proponents => {
+                return mongo.collection('issue')
+                    .updateOne({
+                        'issue.assembly_id': message.body.assembly_id,
+                        'issue.issue_id': message.body.issue_id,
+                        'issue.category': message.body.category,
+                    }, {
+                        $set: {proponents: proponents.slice().sort((first, second) => (first.document.order || 0) - (second.document.order || 0))}
+                    }, {
+                        upsert: true
+                    }).then(result => {
+                        if (!result.result.ok) {
+                            throw new Error(`ERROR: DocumentCongressman.addProponentIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`);
+                        }
+                        return `DocumentCongressman.addProponentIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`;
+                    });
             });
     }
 
     return Promise.resolve(`DocumentCongressman.addProponentIssue: no-action`);
-};
-
-/**
- * When ever a proponent is added to a document, if it's the initial one, then we can update the proponent's count
- * on the Issue
- *
- * @param message
- * @param mongo
- * @param client
- */
-export const addProponentCountIssue = async (message: Message<CongressmanDocument>, mongo: Db, client: HttpQuery): Promise<any> => {
-    return Promise.all([
-        client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol/${message.body.document_id}`),
-        client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol`),
-        client(`/samantekt/loggjafarthing/${message.body.assembly_id}/thingmal/${message.body.issue_id}/thingskjol/${message.body.document_id}/thingmenn`)
-    ]).then(async ([document, documents, count]) => {
-
-        if (documents[0] && documents[0].document_id === message.body.document_id) {
-            return mongo.collection('issue')
-                .updateOne({
-                    'issue.assembly_id': message.body.assembly_id,
-                    'issue.issue_id': message.body.issue_id,
-                    'issue.category': message.body.category,
-                }, {
-                    $set: {proponentCount: count}
-                }, {
-                    upsert: true
-                }).then(result => {
-                    if (!result.result.ok) {
-                        throw new Error(`ERROR: DocumentCongressman.addProponentCountIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`);
-                    }
-                    return `DocumentCongressman.addProponentCountIssue(${message.body.assembly_id}, ${message.body.issue_id}, ${message.body.category}, ${message.body.document_id})`;
-                });
-        }
-        return Promise.resolve(`DocumentCongressman.addProponentCountIssue: no-action`);
-    });
 };
